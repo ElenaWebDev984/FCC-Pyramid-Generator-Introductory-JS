5fef546bc982028b526a0223fde8ff1e1a9f940a
ElenaWebDev984 <badalova.dev.984@gmail.com>
2024-10-09 13:49:26 +0200

    step 30

--- main.js
+++ main.js
@@ -1,21 +1,3 @@
 let character = 'Hello';
-/*console.log(character);
-character = "World";
-console.log(character);
-let secondCharacter;
-console.log(secondCharacter);
-secondCharacter = "Test";
-secondCharacter = character;
-let profession;
-profession = "teacher";
-let age;
-console.log(profession);
-console.log(age);*/
 let count = 8;
-//console.log(count + 1);
-let rows = ["Naomi", "Quincy", "CamperChan"];
-console.log(rows[0]);
-
-
-
-
+let rows = [];
